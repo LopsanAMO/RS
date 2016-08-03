bf254d37eb23126524e2f9ea9e15ca6dcb8b9d6f
LopsanAMO <makiki789@gmail.com>
2016-08-02 20:34:22 -0500

    creacion del modelo de usuario

--- rs/autentificacion/models.py
+++ rs/autentificacion/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Usuario(models.Model):
+    nickname = models.ForeignKey(User, blank=False, null=False)
+    nombre = models.CharField(blank=False, null=False, max_length=50)
+    apellido = models.CharField(blank=False, null=False, max_length=50)
+    correo  = models.EmailField(blank=False, null=False, max_length=50)
+    edad = models.CharField(blank=False, null=False, max_length=50)
+    genero = models.CharField(blank=False, null=False, max_length=50)
+    contra = models.CharField(blank=False, null=False, max_length=50)
+
+    def __str__(self):
+        return "perfil de {}".format(self.nombre)
